f45b743bbc48d52a10608e5e05989649a4577b3b
dev-hafiz <johnstarhafiz@gmail.com>
2024-09-13 23:25:27 +0600

    Responsive check and it's work successfully

--- src/pages/home/success-info/SuccessInfo.jsx
+++ src/pages/home/success-info/SuccessInfo.jsx
@@ -1,23 +1,25 @@
 const SuccessInfo = () => {
   return (
-    <div className="py-10 my-10 bg-[#FAF6EF] px-5 lg:px-80 flex flex-wrap">
-      <div className="w-[400px] px-14 text-center">
-        <h4 className="font-Georama text-[32px] font-medium">90%</h4>
-        <p className="font-Georama text-[24px] font-normal ">
-          of all orders are produced locally
-        </p>
-      </div>
-      <div className="w-[429px] px-14 text-center">
-        <h4 className="font-Georama text-[32px] font-medium">90%</h4>
-        <p className="font-Georama text-[24px] font-normal ">
-          of orders arrive within 5 days of ordering
-        </p>
-      </div>
-      <div className="w-[429px] px-14 text-center">
-        <h4 className="font-Georama text-[32px] font-medium">140+</h4>
-        <p className="font-Georama text-[24px] font-normal ">
-          print providers across 32 countries
-        </p>
+    <div className="lg:block hidden py-10 my-10 bg-[#FAF6EF] px-5 lg:px-80 ">
+      <div className="flex flex-wrap">
+        <div className="w-[400px] px-14 text-center">
+          <h4 className="font-Georama text-[32px] font-medium">90%</h4>
+          <p className="font-Georama text-[24px] font-normal ">
+            of all orders are produced locally
+          </p>
+        </div>
+        <div className="w-[429px] px-14 text-center">
+          <h4 className="font-Georama text-[32px] font-medium">90%</h4>
+          <p className="font-Georama text-[24px] font-normal ">
+            of orders arrive within 5 days of ordering
+          </p>
+        </div>
+        <div className="w-[429px] px-14 text-center">
+          <h4 className="font-Georama text-[32px] font-medium">140+</h4>
+          <p className="font-Georama text-[24px] font-normal ">
+            print providers across 32 countries
+          </p>
+        </div>
       </div>
     </div>
   );
